104c4f9f60f207f412ae4016c2b03a65ae967337
Dhaiwat <dhaiwatpandya@gmail.com>
2019-12-14 05:10:08 +0530

    Added escaping for http and https in url entry

--- routes/urls.js
+++ routes/urls.js
@@ -10,6 +10,26 @@ router.route('/new').post((req, res) => {
     console.log('Created by: '+ req.body.username)
     console.log(newCode);
 
+    let baseUrl = req.body.baseUrl;
+
+    console.log(baseUrl.substring(0, 6));
+
+    if(baseUrl.substring(0, 7) === 'http://') {
+        let arr = baseUrl.split('');
+        arr.splice(0, 7);
+        console.log(arr);
+        baseUrl = arr.join('');     
+    }
+
+    if(baseUrl.substring(0, 8) === 'https://') {
+        let arr = baseUrl.split('');
+        arr.splice(0, 8);
+        console.log(arr);
+        baseUrl = arr.join('');  
+    }
+
+    console.log('Modified base URl: ' + baseUrl);
+
     const newUrl = new Url({baseUrl: req.body.baseUrl, shortenedCode: newCode, createdBy: req.body.username});
     newUrl.save()
         .then(() => {
