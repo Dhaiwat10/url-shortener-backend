66168104b20796f944989b0edc81896162e0058a
Dhaiwat <dhaiwatpandya@gmail.com>
2019-12-14 08:11:52 +0530

    Minor bug fix

--- routes/shortUrls.js
+++ routes/shortUrls.js
@@ -7,7 +7,7 @@ router.route('/:code').get((req, res) => {
 
     Url.findOne({shortenedCode: reqCode}, (err, foundUrl) => {
         if(!err && foundUrl) {
-            res.send('Redirecting to.. http://' + foundUrl.baseUrl);
+            //res.send('Redirecting to.. http://' + foundUrl.baseUrl);
             res.redirect(foundUrl.baseUrl);
         } else {
             res.status(404).json({message: 'Requested page not found.'});
